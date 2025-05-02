239dcd248b3e36c4a10ce6ba709e01cd47b151aa
Harold David Palencia Castro <palenciacastroharolddavid@gmail.com>
2025-05-02 08:04:11 -0500

    Agregue los Jsp, los Servlets y el modelo

---
+++ src/main/java/com/miproyecto/modelo/Usuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miproyecto.modelo;
+
+/**
+ *
+ * @author Personal
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+}
