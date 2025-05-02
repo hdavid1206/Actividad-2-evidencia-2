38ffb62fccb9fe5f5a5081efb30dfabadb17f700
Harold David Palencia Castro <palenciacastroharolddavid@gmail.com>
2025-05-02 09:26:05 -0500

    Realize el Servlet de ConsultaServlet

---
+++ src/main/java/com/miproyecto/servlets/ConsultaServlet.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.miproyecto.servlets;
+
+import com.miproyecto.modelo.Usuario;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Personal
+ */
+@WebServlet(name = "ConsultaServlet", urlPatterns = {"/consulta"})
+public class ConsultaServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        List<Usuario> listaUsuarios = (List<Usuario>) session.getAttribute("listaUsuarios");
+        if (listaUsuarios == null) {
+            listaUsuarios = new ArrayList<>();
+            session.setAttribute("listaUsuarios", listaUsuarios);
+        }
+        
+        String emailBusqueda = request.getParameter("email");
+        
+        if (emailBusqueda != null && !emailBusqueda.isEmpty()){
+            Usuario usuarioEncontrado = null;
+            
+            for (Usuario usaurio : listaUsuarios) {
+                if (usuario.getEmail().equals(emailBusqueda)) {
+                    usuarioEncontrado = usuario;
+                    break;
+                }
+            }
+            request.setAttribute("usuarioBuscado", usuarioEncontrado);
+            request.setAttribute("busquedaRealizada", true);
+        }
+        request.getRequestDispatcher("/consulta.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
